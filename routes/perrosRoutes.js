639335cc9f24519124cf0388649c3b7965bf3d66
MGMike0 <ingmiguelmorangomez@gmail.com>
2018-10-24 09:01:10 -0500

    s

--- routes/perrosRoutes.js
+++ routes/perrosRoutes.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const perrosMiddleware = require('../lib/perrosMiddleware');
-const { datosCompletos, datosPrimitivos } = perrosMiddleware;
 
 const Perro = mongoose.model('perros');
 
@@ -14,8 +13,24 @@ module.exports = (app) =>{
 	res.send(perros);
 	});
 
+	app.get('/api/perros/:id', async (req, res) =>{
+	
+		try{
+			const perros = await Perro.find({ _id: req.params.id });
+			res.send(perros);
+		}catch(error){
+			res.send(error.message)
+		};
 
-	app.post('/api/perros', datosCompletos, datosPrimitivos, async (req, res) => {
+	});
+
+
+	app.post(
+		'/api/perros',
+		perrosMiddleware.datosCompletos,
+		perrosMiddleware.datosPrimitivos, 
+		perrosMiddleware.datosValidos, 
+		async (req, res) => {
 
 		const { nombre, raza, edad, vacunas } = req.body;
 
@@ -26,4 +41,31 @@ module.exports = (app) =>{
 		res.send(respuesta);
 	});
 
+	app.post(
+		'/api/perros/:id',
+		perrosMiddleware.datosCompletos,
+		perrosMiddleware.datosPrimitivos, 
+		perrosMiddleware.datosValidos, 
+		async (req, res) => {
+
+		const { nombre, raza, edad, vacunas } = req.body;
+
+		const respuesta= await Perro.findOneAndUpdate(
+			{_id:req.params.id},
+			{nombre, raza, edad, vacunas},
+			{new: true}
+		).exec();
+		res.send(respuesta);
+	});
+
+	app.delete('/api/perros/:id', async(req, res) =>{
+		try{
+		const perros = await Perro.find({ _id: req.params.id });
+		if(!perros) res.send("Perro no existe")
+		const respuesta = await Perro.deleteOne({ _id: req.params.id });
+		res.send(respuesta);
+	}catch(error){
+		res.send(error.message);
+	}
+	});
 };	
